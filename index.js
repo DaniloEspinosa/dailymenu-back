e377bb074b1318ab5570f92336eb8182848059d7
DaniloEspinosa <115655795+DaniloEspinosa@users.noreply.github.com>
2025-09-04 13:48:11 +0200

    Basic configuration CI/CD azure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
